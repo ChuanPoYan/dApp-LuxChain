117f4d19c7bfe3ff91a462a56f924604b651fee4
koreydai <77912303+koreydaiyang@users.noreply.github.com>
2022-04-12 18:29:41 +0800

    Add the invalidate and steal function

--- contracts/LuxChain.sol
+++ contracts/LuxChain.sol
@@ -6,11 +6,22 @@ contract LuxChain is ERC721 {
     address _owner = msg.sender;
     uint256 _supply;
 
+    enum stages{
+        normal,
+        lost,
+        invalidating,
+        invalidate
+    }
+
     mapping(uint256 => Token) tokens;
     mapping(string => uint256) serialNumbers;
 
     event mintToken(address _to, string _serNumber, uint256 tokenId);
     event transferEvent(address _from, address _to, uint256 _tokenId);
+    event invalidToken(uint256 _tokenId);
+    event tokendestory(uint256 _tokenId);
+    event tokenLost(uint256 _tokenId);
+    event tokenrestored(uint256 _tokenId);
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
@@ -20,6 +31,28 @@ contract LuxChain is ERC721 {
         string serialNumber;
         string name;
         string information;
+        stages state;
+        uint256 time;
+    }
+
+    modifier stateoftoken(uint256 tokenId) {
+        require(tokens[tokenId].state == stages.normal, "Token is abnormal");
+        _;
+    }
+
+    modifier validToken(uint256 tokenId) {
+        require(tokens[tokenId].state != stages.invalidate, "Token is invalidated already");
+        _;
+    }
+
+    modifier timedDestory(uint256 _tokenId) {
+        require(tokens[_tokenId].state == stages.normal, "Token is abnormal");
+        if (tokens[_tokenId].state == stages.invalidating && block.timestamp >= tokens[_tokenId].time + 8 days) {
+            tokens[_tokenId].state = stages.invalidate;
+            super._burn(_tokenId);
+            emit tokendestory(_tokenId);
+        }
+        _;
     }
 
     modifier adminOnly {
@@ -35,13 +68,33 @@ contract LuxChain is ERC721 {
     // When a product is sold, an accompanying token created
     function mint(address _to, string memory _serialNumber, string memory _name) public adminOnly {
         super._mint(_to, _supply);
-        Token memory newToken = Token(_serialNumber, _name, '');
+        Token memory newToken = Token(_serialNumber, _name, '', stages.normal, block.timestamp);
         tokens[_supply] = newToken;
         serialNumbers[_serialNumber] = _supply++;
         emit mintToken(_to, _serialNumber, _supply);
     }
 
-    function transferFrom(address _from, address _to, uint256 _tokenId) public override adminOrOwnerOnly(_tokenId) {
+    function invalidateToken(uint256 _tokenId) public adminOnly timedDestory(_tokenId){
+        tokens[_tokenId].state = stages.invalidating;
+        tokens[_tokenId].time = block.timestamp;
+        emit invalidToken(_tokenId);
+    }
+
+    function reportlost(uint256 _tokenId) public adminOrOwnerOnly(_tokenId) stateoftoken(_tokenId) {
+        tokens[_tokenId].state = stages.lost;
+        emit tokenLost(_tokenId);
+    }
+
+    function restoreToken(uint256 _tokenId) public adminOnly validToken(_tokenId) {
+        tokens[_tokenId].state = stages.normal;
+        emit tokenrestored(_tokenId);
+    }
+
+    function checkState(uint256 _tokenId) public view returns(stages) {
+        return tokens[_tokenId].state;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) public override adminOrOwnerOnly(_tokenId) stateoftoken(_tokenId) {
         super.transferFrom(_from, _to, _tokenId);
         tokens[_tokenId].information = '';
         emit transferEvent(_from, _to, _tokenId);
